bea2ebbb08fedf54511a231c0e6da79b6d371fc6
Adam <58306405+adamafriansyahb@users.noreply.github.com>
2021-01-15 23:46:33 +0700

    Add some comments

--- reverseInt.js
+++ reverseInt.js
@@ -2,12 +2,19 @@ const upperBound = 2**31;
 const lowerBound = upperBound * -1;
 
 const reverse = (num) => {
+    // make the input number into positive, convert it into string, then split.
     let result = String(Math.abs(num)).split("");
+    
+    // reverse the splitted string, then concatenate them.
     result = result.reverse().join("");
+    // revert the string into number.
     result = Number(result);
-        
+    
+    // check whether the input is positive or negative.
     if (num < 0) {
+        // since the input is negative, the returned number must also be negative.
         result = result * -1;
+        // check whether it follows the constraints or not.
         if (result >= lowerBound && result <= upperBound) {
             return result;
         } 
@@ -26,4 +33,5 @@ const reverse = (num) => {
 
 }
 
+// run the function.
 console.log(reverse(-1534236469)); 
