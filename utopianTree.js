45229fb364a277671e80676d55da6872bb5f6efd
Adam <58306405+adamafriansyahb@users.noreply.github.com>
2021-03-18 16:03:08 +0700

    Add files via upload

---
+++ utopianTree.js
@@ -0,0 +1,13 @@
+const utopianTree = (n) => {
+    let height = 1;
+    for (let i=1; i<=n; i++) {
+        if (i % 2 == 0) {
+            height += 1
+        }
+        else {
+            height *= 2
+        }
+    }
+
+    return height;
+}
