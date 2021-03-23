6a656a7d455edfdf51c7322dc459fb13aeddac31
Adam <58306405+adamafriansyahb@users.noreply.github.com>
2021-03-23 21:17:33 +0700

    Update and rename equalizeTheArray.py to equalize_the_array.py

--- equalize_the_array.py
+++ equalize_the_array.py
@@ -1,16 +1,13 @@
 def equalizeArray(arr):
     hehe = {}
     new_arr = []
+    
     for i in arr:
         if i not in hehe:
             hehe[i] = 1
             new_arr.append(arr.count(i))
 
-    maximum_count = max(new_arr)
-    new_arr.sort()
-    new_arr.pop()
-
-    return sum(new_arr)
+    return len(arr) - max(highest)
 
 
 arr = [1, 2, 2, 3]
