6082cc35981645b25f612272666ca047ba37e2ee
Adam <58306405+adamafriansyahb@users.noreply.github.com>
2021-05-04 21:48:30 +0700

    Create string_n_int_multiplication.py

---
+++ string_n_int_multiplication.py
@@ -0,0 +1,13 @@
+def solution(D, S):
+    string = S
+
+    if string == "one":
+        return D * 1
+    elif string == "two":
+        return D * 2
+    elif string == "three":
+        return D * 3
+    elif string == "four":
+        return D * 4
+    elif string == "five":
+        return D * 5
